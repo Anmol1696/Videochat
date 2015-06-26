287d49bbd1c6848dffb7af21e82d2f7619e8f9b6
siddharth <siddharth140010017@github.com>
2015-06-26 13:19:41 +0530

    TCP protocol but very slow

--- client_video.py
+++ client_video.py
@@ -11,34 +11,31 @@ def chunk_image(chunks):
 		out +=  chunk
 	out = decompress(out)
 	sr = pickle.loads(out)
+	print type(sr)
 	return sr
 
 def Rev():
-	addr = ('127.0.0.9',5000)
+	addr = ('127.0.0.8',5000)
 	s = socket()
 	s.connect(addr)
-	out = []
 	
-	pre = s.recv(12)
-	print pre
-	(num_chunks, len_last) = (int(pre.split(',')[0]), int(pre.split(',')[1]))
-	
-	print 'now'
-	
-	for rond in range(num_chunks):
-		if rond != num_chunks - 1:
-			chunk= s.recv(2**15)
-			out.append(chunk)
-		else:
-			chunk= s.recv(len_last)
-			out.append(chunk)
-		print 'step'
-	return out
-
-def show(frame):
 	cv2.namedWindow('Chat')
-	cv2.imshow('Chat', frame)
-	print frame
+	
+	while True:
+		out = []
+		pre = s.recv(12)
+		(num_chunks, len_last) = (int(pre.split(',')[0]), int(pre.split(',')[1]))
+		for rond in range(num_chunks):
+			if rond != num_chunks - 1:
+				chunk= s.recv(2**17)
+				out.append(chunk)
+			else:
+				chunk= s.recv(len_last)
+				out.append(chunk)
+		cv2.imshow('Chat', chunk_image(out))
+		key = cv2.waitKey(20)
+		print 'fine'
+
 
 if __name__ == '__main__':
-	show(chunk_image(Rev()))
+	Rev()
--- server_video.py
+++ server_video.py
@@ -5,35 +5,34 @@ from bz2 import compress
 
 
 def chunks(inp):
-	bs = 2**15
+	sr = dumps(inp)
+	inp = compress(sr)
+	bs = 2**17
 	out = []
 	for x in range((len(inp) / bs) + 1):
 		out.append(inp[(bs*x):(bs)*(x+1)])
 	return out
 
-def Ser(inp):
-	addr = ('127.0.0.9', 5000)
+def Ser():
+	addr = ('127.0.0.8', 5000)
 	s = socket()
 	s.bind(addr)
 	
 	s.listen(1)
 	c, addr = s.accept()
 	
-	sr = dumps(inp)
-	data = compress(sr)
-	ch = chunks(data)
-	pre = str(len(ch)) + ',' + str(len(ch[-1])) + ','
-	pre = pre.ljust(12)
-	c.send(pre)
-	for x in ch:
-		c.send(x)
-
-def Frame():
 	vc = cv2.VideoCapture(0)
-	_, frame = vc.read()
-	vc.release()
-	print frame
-	return frame
+	
+	while True:
+		_, frame = vc.read()
+		ch = chunks(frame)
+		pre = str(len(ch)) + ',' + str(len(ch[-1])) + ','
+		pre = pre.ljust(12)
+		c.send(pre)
+		for x in ch:
+			c.send(x)
+		print 'fine'
+
 
 if __name__ == '__main__':
-	Ser(Frame())
+	Ser()
